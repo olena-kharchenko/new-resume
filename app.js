50d2156c87c61cd0c51a5bbf904714dd047c8af8
olena-kharchenko <harchenko.es1990@gmail.com>
2020-10-16 21:03:47 +0300

    feat: complite resume in en

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exhbs = require('express-handlebars');
 
-const PORT = process.env.PORT || 4444;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
